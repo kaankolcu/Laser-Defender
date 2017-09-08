3ed09792c324ce904d087762411df009a0ef43de
k1502722 <kaan.kolcu@kcl.ac.uk>
2017-09-08 16:58:33 +0100

    Added Player ship and left/right mouse key movability

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 15.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (-speed * Time.deltaTime, 0,0);
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (speed * Time.deltaTime, 0,0);
+		}
+	}
+}
