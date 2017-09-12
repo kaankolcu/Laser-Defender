7d4b9b0eed22a931984b5d172b8ac625c5524015
k1502722 <kaan.kolcu@kcl.ac.uk>
2017-09-12 10:41:41 +0100

    Constrained movement to playspace, so player ship doesn't go off the grid.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -6,17 +6,34 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 
+	public float xMin = -5;
+	public float xMax = 5;
+
+	public float padding = 1f;
+
 	// Use this for initialization
 	void Start () {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3 (0,0,distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3 (1,0,distance));
+
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+
 		speed = 15.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.position += new Vector3 (-speed * Time.deltaTime, 0,0);
+
+			transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.position += new Vector3 (speed * Time.deltaTime, 0,0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		float newX = Mathf.Clamp (transform.position.x,xMin,xMax);
+
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
 	}
 }
