f563e8c09a9ab803e1bf3c4f138dbe5b1fd08c8a
k1502722 <kaan.kolcu@kcl.ac.uk>
2017-09-12 16:37:26 +0100

    added functionality for enemies to slide left and right of the play space.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,7 +26,6 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-
 			transform.position += Vector3.left * speed * Time.deltaTime;
 		} else if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
